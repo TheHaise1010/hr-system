a609e6e0e4eb6b1844f2f600b1ec165268ba664b
TheHaise <jordan.zelayahs@gmail.com>
2024-08-06 20:26:18 -0600

    POJO Departamento

--- src/main/java/com/example/mywebapp/model/Departamento.java
+++ src/main/java/com/example/mywebapp/model/Departamento.java
@@ -1,4 +1,31 @@
 package com.example.mywebapp.model;
 
 public class Departamento {
+    private int idDepartamento;
+    private String nombreDepartamento;
+    private String descripcionDepartamento;
+
+    public int getIdDepartamento() {
+        return idDepartamento;
+    }
+
+    public void setIdDepartamento(int idDepartamento) {
+        this.idDepartamento = idDepartamento;
+    }
+
+    public String getNombreDepartamento() {
+        return nombreDepartamento;
+    }
+
+    public void setNombreDepartamento(String nombreDepartamento) {
+        this.nombreDepartamento = nombreDepartamento;
+    }
+
+    public String getDescripcionDepartamento() {
+        return descripcionDepartamento;
+    }
+
+    public void setDescripcionDepartamento(String descripcionDepartamento) {
+        this.descripcionDepartamento = descripcionDepartamento;
+    }
 }
