e59ed06f00da470702dd90339a5a7603db51bb9c
TheHaise <jordan.zelayahs@gmail.com>
2024-08-06 20:39:39 -0600

    Added POJO Cargo

--- src/main/java/com/example/mywebapp/model/Cargo.java
+++ src/main/java/com/example/mywebapp/model/Cargo.java
@@ -1,4 +1,43 @@
 package com.example.mywebapp.model;
 
 public class Cargo {
+    private int idCargo;
+    private String cargo;
+    private String descripcionCargo;
+    private boolean jefatura;
+
+    public int getIdCargo() {
+        return idCargo;
+    }
+
+    public void setIdCargo(int idCargo) {
+        if (idCargo <= 0) {
+            throw new IllegalArgumentException("ID debe ser positivo");
+        }
+        this.idCargo = idCargo;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getDescripcionCargo() {
+        return descripcionCargo;
+    }
+
+    public void setDescripcionCargo(String descripcionCargo) {
+        this.descripcionCargo = descripcionCargo;
+    }
+
+    public boolean isJefatura() {
+        return jefatura;
+    }
+
+    public void setJefatura(boolean jefatura) {
+        this.jefatura = jefatura;
+    }
 }
